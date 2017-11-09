fa4eeb14de616dacf6eb210a7da7ffaacaeda487
alexsimaschevici <32359205+alexsimaschevici@users.noreply.github.com>
2017-11-09 12:01:05 +0100

    FirstBuild
    The 01 kickstart

---
+++ StudentForum/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentForum.MvcApplication" Language="C#" %>
---
+++ StudentForum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentForum.Startup))]
+namespace StudentForum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
