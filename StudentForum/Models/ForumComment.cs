127ca0ad3e71afe42e76ee76a764395855e7541d
S. Rasmussen <siras@outlook.com>
2017-11-16 11:14:31 +0100

    Add model

---
+++ StudentForum/Models/ForumComment.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentForum.Models
+{
+   public class ForumComment
+   {
+
+      public long Id { get; set; }
+      public virtual ApplicationUser Author { get; set; }
+      public string Content { get; set; }
+   }
+}
---
+++ StudentForum/Models/ForumThread.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StudentForum.Models;
+
+namespace StudentForum.Models
+{
+   public class ForumThread
+   {
+      public long Id { get; set; }
+      public string Title { get; set; }
+      public string Content { get; set; }
+      public virtual ApplicationUser Author { get; set; }
+      public ICollection<string> FilePaths { get; set; }
+      public virtual ICollection<ForumComment> Comments { get; set; }
+
+   }
+}
